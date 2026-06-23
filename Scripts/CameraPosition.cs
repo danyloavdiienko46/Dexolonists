d4497f7723ce1ed2410eae002b5d038ed11ad8f7
danyloavdiienko46 <danyloavdiienko@gmail.com>
2026-06-23 18:11:44 +0200

    Added camera movement in Map Creator

---
+++ Scripts/CameraPosition.cs
@@ -0,0 +1,73 @@
+using Godot;
+using System;
+using System.Runtime.CompilerServices;
+
+public partial class CameraPosition : Camera3D
+{
+	[Export] public float MoveSpeed = 0.6f;
+	[Export] public float RotateKeysSpeed = 1.5f;
+	[Export] public float ZoomSpeed = 3.0f;
+	[Export] public float MinZoom = -20.0f;
+	[Export] public float MaxZoom = 20.0f;
+	[Export] public float MouseSensitivity = 0.3f;
+
+	private Vector3 _move_target;
+	private float _rotate_keys_target;
+	private float _zoom_target;
+
+	public override void _Ready()
+	{
+		_move_target = Position;
+		_rotate_keys_target = RotationDegrees.Y;
+		_zoom_target = Position.Z;
+	}
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion ev && Input.IsActionPressed("rotate"))
+		{
+			_rotate_keys_target -= ev.Relative.X * MouseSensitivity;
+
+			Vector3 rot_degrees = RotationDegrees;
+			rot_degrees.X -= ev.Relative.Y * MouseSensitivity;
+			rot_degrees.X = Mathf.Clamp(rot_degrees.X, -90, 90);
+			RotationDegrees = rot_degrees;
+		}
+    }
+
+
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("rotate"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+		}
+		else if (Input.IsActionJustReleased("rotate"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
+
+		Vector2 input_direction = Input.GetVector("left", "right", "up", "down");
+		Vector3 movement_direction = 
+		(Transform.Basis * new Vector3(input_direction.X, 0, input_direction.Y)).Normalized();
+
+		float rotate_keys = Input.GetAxis("rotate_left", "rotate_right");
+
+		int zoom_dir = Convert.ToInt32(Input.IsActionJustReleased("zoom_out"))
+		 - Convert.ToInt32(Input.IsActionJustReleased("zoom_in"));
+
+		_move_target += movement_direction*MoveSpeed;
+		_rotate_keys_target += rotate_keys*RotateKeysSpeed;
+		_zoom_target += zoom_dir*ZoomSpeed;
+
+		Position = Position.Lerp(_move_target, 0.25f);
+
+		Vector3 rot = RotationDegrees;
+		rot.Y = Mathf.Lerp(rot.Y, _rotate_keys_target, 1.0f);
+		RotationDegrees = rot;
+
+		Vector3 cam_pos = Position;
+		cam_pos.Z = Mathf.Lerp(cam_pos.Z, _zoom_target, 0.1f);
+		Position = cam_pos;
+	}
+}
